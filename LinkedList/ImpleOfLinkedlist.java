bd5a4a68aea48c08d3c653b394bde4e000ae8508
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-08-24 00:42:10 +0530

    practicing question on linkedlist

--- LinkedList/ImpleOfLinkedlist.java
+++ LinkedList/ImpleOfLinkedlist.java
@@ -1,25 +1,38 @@
 public class ImpleOfLinkedlist {
+
     Node<Integer> head;
 
     ImpleOfLinkedlist() {
         this.head = null;
     }
 
-
-
-
-
-    public void InsertHead(int data){
+    public void InsertHead(int data) {
         Node<Integer> newNode = new Node(data);
 
-        if(head==null){
-            newNode= head;
+        if (head == null) {
+            newNode = head;
             System.out.println("Insertion Successfull.....");
             return;
+
         }
         newNode.next = head;
         head = newNode;
         System.out.println("Insertion Successfull.....");
+    }
+
+    public void InsertTail(int data){
+        Node<Integer> newNode = new Node(data);
+        if(head==null){
+            newNode= head;
+            System.out.println("Insertion Successfull.....");
+            return;
+        } 
+        Node<Integer> temp = head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next = newNode;
+        System.out.println("Insertion Successfull.....");   
     }
 
 }
