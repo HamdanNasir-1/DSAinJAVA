9514642d05d6f9bda698f6ed7c7e91edfd38b5ff
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-08-24 00:56:33 +0530

    insertion at between

--- LinkedList/ImpleOfLinkedlist.java
+++ LinkedList/ImpleOfLinkedlist.java
@@ -20,19 +20,39 @@ public class ImpleOfLinkedlist {
         System.out.println("Insertion Successfull.....");
     }
 
-    public void InsertTail(int data){
+    public void InsertTail(int data) {
         Node<Integer> newNode = new Node(data);
-        if(head==null){
-            newNode= head;
+        if (head == null) {
+            newNode = head;
             System.out.println("Insertion Successfull.....");
             return;
-        } 
+        }
         Node<Integer> temp = head;
-        while(temp.next!=null){
-            temp=temp.next;
+        while (temp.next != null) {
+            temp = temp.next;
         }
         temp.next = newNode;
-        System.out.println("Insertion Successfull.....");   
+        System.out.println("Insertion Successfull.....");
+    }
+
+    public void InsertAtBetween(int data, int index) {
+        if (index == 0) {
+            InsertHead(data);
+            System.out.println("Insertion Successfull.....");
+        } else if (index == index + 1) {
+            InsertTail(data);
+            System.out.println("Insertion Successfull.....");
+        } else {
+            int counter = 0;
+            Node<Integer> newNode = new Node(data);
+            Node<Integer> temp = head;
+            while (counter < index - 1 && temp != null) {
+                counter++;
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
     }
 
 }
