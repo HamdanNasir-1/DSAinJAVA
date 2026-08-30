2abbbff449e47908356e5f5a23c4e69da5fad7b2
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-08-30 23:41:46 +0530

    practicing dsa

--- LinkedList/pracitce.java
+++ LinkedList/pracitce.java
@@ -1,12 +1,17 @@
 public class pracitce {
-    
+
 }
+
 class Node<T> {
     T data;
-   Node next;
-   
-   public Node (){
+    Node next;
 
-   }
-   
+    public Node() {
+
+    }
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
 }
